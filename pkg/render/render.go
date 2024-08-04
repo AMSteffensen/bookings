707c59a2334fa9b4c62c6e4567608b01c85bb69a
Andreas Steffensen <andreas.steffensen89@gmail.com>
2024-08-04 10:37:13 +0200

    added post request route to search availability

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,9 +8,9 @@ import (
 	"net/http"
 	"path/filepath"
 
-
 	"github.com/AMSteffensen/bookings/pkg/config"
 	"github.com/AMSteffensen/bookings/pkg/models"
+	"github.com/justinas/nosurf"
 )
 
 var functions = template.FuncMap{}
@@ -22,13 +22,13 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
-
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 // RenderTemplate renders a template
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 
 	if app.UseCache {
@@ -45,7 +45,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	_ = t.Execute(buf, td)
 
